3c5a0258629f2da8f95e53386354687b8ea4f076
anngineery <by.ann.yun@gmail.com>
2024-01-16 00:56:11 -0500

    class and class var declaration completed and working

--- compiler/JackAnalyzer.py
+++ compiler/JackAnalyzer.py
@@ -1,14 +1,14 @@
 """
 Jack syntax analyzer that converts the high-level Jack language into the intermediate VM language.
 It does not handle error reporting as it assumes all input conforms to Jack language grammar perfectly.
-The input to JackAnalyzer can be a single file (x.jack) or
-a directory that contains 1+ jack files. 
-For each jack file, an output file called x.xml should be created.
+The input to JackAnalyzer can be a single file (x.jack) or a directory that contains 1+ jack files. 
+For each .jack file, an output file should have the same name with .xml extension.
 """
 from pathlib import Path
 import sys
-from compiler.tokenizer import Tokenizer
-from typing import List
+from tokenizer import Tokenizer
+from compilation_engine import CompilationEngine
+from typing import List, Tuple
 
 
 def get_input_files(path: Path) -> List[Path]:
@@ -29,23 +29,38 @@ if __name__ == "__main__":
     files_to_translate = get_input_files(fp)
 
     for file in files_to_translate:
+        token_stream: List[Tuple[str]] = [] # will be filled out by the tokenizer
+        output_token_stream = []    # will be filled out by the compilation engine
         output_file_name = file.with_suffix(".xml")
-        tokenizer = Tokenizer(file) # is it the full path?
-        with open(output_file_name, "w") as output_file:
-            output_file.write("<tokens>" + "\n")
+        tokenizer = Tokenizer(file)
 
-            while tokenizer.has_more_tokens():
-                token, type = tokenizer.get_token_and_type()
-                # convert some symbols used by XMLs to display properly
-                if token == "<":
-                    token = "&lt;"
-                elif token == ">":
-                    token = "&gt;"
-                elif token == "\"":
-                    token = "&quot;"
-                elif token == "&":
-                    token = "&amp;"
-                output_file.write(f"<{type}>{token}</{type}>" + "\n")
-                tokenizer.advance()
+        # first, the tokenizer extracts the stream of tokens from the input file
+        while tokenizer.has_more_tokens():
+            token, type = tokenizer.get_token_and_type()
 
-            output_file.write("</tokens>" + "\n")
+            # convert some symbols used by XMLs to display properly
+            if token == "<":
+                token = "&lt;"
+            elif token == ">":
+                token = "&gt;"
+            elif token == "\"":
+                token = "&quot;"
+            elif token == "&":
+                token = "&amp;"
+
+            token_stream.append((token, type))
+            tokenizer.advance()
+
+        # second, the compilation engine structures tokens into a program
+        print(token_stream)
+        engine = CompilationEngine(token_stream, output_token_stream)
+        print(output_token_stream)
+
+        # third, the final xml is generated
+
+"""
+tokenizer's only responsibility is to break down the input file into tokens.
+compilation engine's only responsbility is to structure token streams into a proper program
+structure using the language grammar specification.
+JackAnalyzer handles orchestration and file stuff.
+"""
--- compiler/compilation_engine.py
+++ compiler/compilation_engine.py
@@ -1,13 +1,62 @@
+"""
+Use recursive descent parsing to check a stream of valid tokens against Jack language grammar.
+"""
+from typing import List, Tuple
+from enum import Enum
+from tokenizer import TokenType
+
+
+class ProgramConstructType(str, Enum):
+    CLASS = "class"
+    CLASS_VAR_DECLARATION = "classVarDec"
+    SUBROUTINE_DECLARATION = "subroutineDec"
+    # TODO: more to be added
+
+
 class CompilationEngine:
-    def __init__(self):
-       pass
+    def __init__(self, token_stream: List[Tuple[str]], output_stream: List[str]):
+       self.input = token_stream
+       self.output = output_stream
+       self.current_token_index = 0
 
-    def compile_class():
-        pass
+       self.compile_class()
 
-    def compile_class_var_declaration():
-        pass
+    def _process_terminal_token(self):
+        token, type = self.input[self.current_token_index]
+        self.output.append(f"<{type}>{token}</{type}>")
+        self.current_token_index += 1
 
+    def compile_class(self):
+        self.output.append(f"<{ProgramConstructType.CLASS}>")
+        self._process_terminal_token()  # "class"
+        self._process_terminal_token()  # class_name
+        self._process_terminal_token()  # "{"
+
+        # handling 0 or more class variable declaration or subroutine declaration
+        while self.input[self.current_token_index] != ("}", TokenType.SYMBOL):
+            token, _ = self.input[self.current_token_index]
+            if token in ["static", "field"]:
+                self.compile_class_var_declaration()
+            elif token in ["constructor", "function", "method"]:
+                self.compile_subroutine()
+
+        self._process_terminal_token()  # "}"
+        self.output.append(f"</{ProgramConstructType.CLASS}>")
+
+    def compile_class_var_declaration(self):
+        self.output.append(f"<{ProgramConstructType.CLASS_VAR_DECLARATION}>")
+        self._process_terminal_token()  # static or field
+        self._process_terminal_token()  # type
+        self._process_terminal_token()  # variable name
+
+        # handling optional (0 or more) piece
+        while self.input[self.current_token_index] == (",", TokenType.SYMBOL):
+            self._process_terminal_token()  # ","
+            self._process_terminal_token()  # variable name
+        
+        self._process_terminal_token()  # ";"
+        self.output.append(f"</{ProgramConstructType.CLASS_VAR_DECLARATION}>")
+        
     def compile_subroutine():
         pass
 
@@ -43,3 +92,7 @@ class CompilationEngine:
 
     def compile_expression_list():
         pass
+
+
+# if it's a terminal, then just use the given type and the value itself and make the marked-up output
+    
